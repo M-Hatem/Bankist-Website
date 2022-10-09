3619e0323e4d7e00ec7b63d92216734bd4beb5ca
MadoPlus <ma7modmado@gmail.com>
2022-10-09 14:54:18 +0200

    Added smooth scrolling in navbar and added functionality to tabbed component

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +29,43 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+// To make smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab--container');
+const operationContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', e => {
+  const btn = e.target.closest('.operations__tab');
+  if (!btn) return;
+
+  // To change btns
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  btn.classList.add('operations__tab--active');
+
+  // To change content
+  operationContent.forEach(c =>
+    c.classList.remove('operations__content--active')
+  );
+  const index = btn.dataset.tab;
+  const activeContent = document.querySelector(
+    `.operations__content--${index}`
+  );
+  activeContent.classList.add('operations__content--active');
 });
